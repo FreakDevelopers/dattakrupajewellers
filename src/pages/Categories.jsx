23c55defa6118ff0c413d24523218e3a93e18620
amrutgawade <amrutgawade.23@gmail.com>
2023-12-02 04:44:58 +0530

    feat: ProdctPreview added

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ProductPreview from '../components/Products/ProductPreview'
 
 function Categories() {
   return (
-    <div>Categories</div>
+    <div>
+      <ProductPreview />
+    </div>
   )
 }
 
